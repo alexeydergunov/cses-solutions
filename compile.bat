c835e992853d63a2ead4175924da98925d9d2c30
Alexey Dergunov <alexey.dergunov@gmail.com>
2020-09-27 11:18:50 +0300

    Add compile.bat

---
+++ compile.bat
@@ -0,0 +1,25 @@
+@echo off
+
+g++ ^
+  -Wall ^
+  -Wextra ^
+  -pedantic ^
+  -std=c++17 ^
+  -O2 ^
+  -Wshadow ^
+  -Wformat=2 ^
+  -Wfloat-equal ^
+  -Wconversion ^
+  -Wlogical-op ^
+  -Wshift-overflow=2 ^
+  -Wduplicated-cond ^
+  -Wcast-qual ^
+  -Wcast-align ^
+  -D_GLIBCXX_DEBUG ^
+  -D_GLIBCXX_DEBUG_PEDANTIC ^
+  -D_FORTIFY_SOURCE=2 ^
+  -fno-sanitize-recover ^
+  -fstack-protector ^
+  a.cpp
+
+echo Compilation done
